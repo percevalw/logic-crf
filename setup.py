e8c5704787e0b2430f21c3988fc2b79f29d516b5
Perceval Wajsburt <perceval.wajsburt@gmail.com>
2020-03-11 11:37:05 +0100

    more packaging

--- setup.py
+++ setup.py
@@ -3,22 +3,33 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="logic_crf",  # Replace with your own username
-    version="0.0.1",
-    author="Perceval Wajsburt",
-    author_email="perceval.wajsburt@sorbonne-universite.fr",
-    description="Logic Conditional Random Field",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/perceval/logic_crf",
-    packages=setuptools.find_packages(),
-    package_data={},
-    include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="logic_crf",  # Replace with your own username
+        version="0.0.1",
+        license='BSD 3-Clause',
+        author="Perceval Wajsburt",
+        author_email="perceval.wajsburt@sorbonne-universite.fr",
+        description="Logic Conditional Random Field",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/perceval/logic_crf",
+        packages=setuptools.find_packages(),
+        package_data={},
+        install_requires=[
+            'opt_einsum>=3.1.0',
+            'numpy>=1.17.4',
+            'pandas>=0.24.1',
+            'pyeda>=0.28.0',
+            'torch>=1.3.1',
+        ],
+        include_package_data=True,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+    )
