6b1e29c315f56d8012ccd43f9a4dabc0e3f9fc38
Perceval Wajsburt <perceval.wajsburt@gmail.com>
2020-04-16 22:36:29 +0200

    fixed cuda support

--- logic_crf/utils.py
+++ logic_crf/utils.py
@@ -40,7 +40,7 @@ def flatten_array(array, mask=None):
         raise Exception(f"Unrecognized array type {repr(type(array))} during array flattening (mask type is {repr(type(mask))}')")
 
 
-def factorize(values, mask=None, reference_values=None, freeze_reference=True, keep_old_values=False):
+def factorize(values, mask=None, reference_values=None, freeze_reference=True):
     """
     Express values in "col" as row numbers in a reference list of values
     The reference values list is the deduplicated concatenation of preferred_unique_values (if not None) and col
@@ -53,15 +53,15 @@ def factorize(values, mask=None, reference_values=None, freeze_reference=True, k
 
     Parameters
     ----------
-    values: np.ndarray or torch.Tensor or list of (np.ndarray or torch.Tensor) or list of any
+    col: np.ndarray or scipy.sparse.spmatrix or torch.Tensor or list of (np.ndarray or scipy.sparse.spmatrix or torch.Tensor)
         values to factorize
-    mask: np.ndarray or torch.Tensor or list of (np.ndarray or torch.Tensor) or None
+    mask: np.ndarray or scipy.sparse.spmatrix or torch.Tensor or list of (np.ndarray or scipy.sparse.spmatrix or torch.Tensor) or None
         optional mask on col, useful for multiple dimension values arrays
     freeze_reference: bool
         Should we throw out values out of reference values (if given).
         Then we need a mask to mark those rows as disabled
         TODO: handle cases when a mask is not given
-    reference_values: np.ndarray or torch.Tensor or list or None
+    reference_values: np.ndarray or scipy.sparse.spmatrix or torch.Tensor or list or None
         If given, any value in col that is not in prefered_unique_values will be thrown out
         and the mask will be updated to be False for this value
 
@@ -86,27 +86,40 @@ def factorize(values, mask=None, reference_values=None, freeze_reference=True, k
     for values, mask in zip(all_values, all_masks):
         assert (
               (isinstance(mask, np.ndarray) and isinstance(values, np.ndarray)) or
+              (issparse(mask) and issparse(values)) or
               (torch.is_tensor(mask) and torch.is_tensor(values)) or
-              (mask is None and (isinstance(values, (list, tuple, np.ndarray)) or torch.is_tensor(values)))), (
-            f"values and (optional mask) should be of same type torch.tensor, numpy.ndarray. Given types are values: {repr(type(values))} and mask: {repr(type(mask))}")
+              (mask is None and (isinstance(values, (list, tuple, np.ndarray)) or issparse(values) or torch.is_tensor(values)))), (
+            f"values and (optional mask) should be of same type torch.tensor, numpy.ndarray or scipy.sparse.spmatrix. Given types are values: {repr(type(values))} and mask: {repr(type(mask))}")
         all_flat_values.append(flatten_array(values, mask))
         # return all_values[0], all_masks[0], all_values[0].tocsr().data if hasattr(all_values[0], 'tocsr') else all_values#col.tocsr().data if hasattr(col, 'tocsr')
-    if torch.is_tensor(all_flat_values[0]):
+
+    device = all_flat_values[0].device if torch.is_tensor(all_flat_values[0]) else None
+    if sum(len(vec) for vec in all_flat_values) == 0:
+        relative_values = all_flat_values[0]
+        unique_values = all_flat_values[0]
+    elif torch.is_tensor(all_flat_values[0]):
+        device = all_flat_values[0].device
         if reference_values is None:
-            unique_values, relative_values = torch.unique(torch.cat(all_flat_values), return_inverse=True)
+            unique_values, relative_values = torch.unique(torch.cat(all_flat_values).unsqueeze(0), sorted=False, return_inverse=True)
+        elif freeze_reference:
+            relative_values, unique_values = torch.unique(torch.cat((reference_values, *all_flat_values)).unsqueeze(0), sorted=False, return_inverse=True)[1], reference_values
         else:
-            relative_values, unique_values = torch.unique(torch.cat((reference_values, *all_flat_values)), return_inverse=True)[1], reference_values
+            unique_values, relative_values = torch.unique(torch.cat((reference_values, *all_flat_values)).unsqueeze(0), sorted=False, return_inverse=True)
+        relative_values = relative_values.squeeze(0)
     else:
         if reference_values is None:
             relative_values, unique_values = pd.factorize(np.concatenate(all_flat_values))
-        else:
+        elif freeze_reference:
             relative_values, unique_values = pd.factorize(np.concatenate((reference_values, *all_flat_values)))[0], reference_values
+        else:
+            relative_values, unique_values = pd.factorize(np.concatenate((reference_values, *all_flat_values)))
+
     if freeze_reference:
         all_unk_masks = relative_values < len(reference_values)
     else:
         all_unk_masks = None
 
-    offset = len(reference_values) if reference_values is not None else 0
+    offset = 0 if reference_values is None else len(reference_values)
     new_flat_values = []
     new_flat_values = []
     unk_masks = []
@@ -123,7 +136,21 @@ def factorize(values, mask=None, reference_values=None, freeze_reference=True, k
     new_values = []
     new_masks = []
     for values, mask, flat_relative_values, unk_mask in zip(all_values, all_masks, all_flat_values, unk_masks):
-        if isinstance(values, (list, tuple)):
+        if issparse(values):
+            values = values.tocsr()
+            values.data = flat_relative_values + 1
+            if unk_mask is not None:
+                values.data[~unk_mask] = 0
+            values.eliminate_zeros()
+            new_mask = values.copy()
+            values.data -= 1
+            new_mask.data = np.ones(len(new_mask.data), dtype=bool)
+
+            if mask is not None:
+                new_mask = new_mask.multiply(mask.tocsr())
+            new_values.append(values.tolil())
+            new_masks.append(new_mask.tolil())
+        elif isinstance(values, (list, tuple)):
             mask = unk_mask
             if mask is not None:
                 values = [v for v, valid in zip(flat_relative_values, mask) if valid]
@@ -139,10 +166,11 @@ def factorize(values, mask=None, reference_values=None, freeze_reference=True, k
                 if mask is None:
                     new_mask = unk_mask.reshape(values.shape)
                 else:
-                    new_mask = mask & unk_mask.reshape(mask.shape)
+                    new_mask = mask.copy()
+                    mask[mask] = unk_mask
             if mask is not None:
                 values = np.zeros(values.shape, dtype=int)
-                values[mask] = flat_relative_values
+                values[mask] = flat_relative_values[unk_mask] if unk_mask is not None else flat_relative_values
                 new_values.append(values)
                 new_masks.append(new_mask)
             else:
@@ -155,10 +183,11 @@ def factorize(values, mask=None, reference_values=None, freeze_reference=True, k
                 if mask is None:
                     new_mask = unk_mask.view(*values.shape)
                 else:
-                    new_mask = mask & unk_mask.view(*mask.shape)
+                    new_mask = mask.clone()
+                    mask[mask] = unk_mask
             if mask is not None:
-                values = torch.zeros(values.shape, dtype=torch.long)
-                values[mask] = flat_relative_values
+                values = torch.zeros(values.shape, dtype=torch.long, device=device)
+                values[mask] = flat_relative_values[unk_mask] if unk_mask is not None else flat_relative_values
                 new_values.append(values)
                 new_masks.append(mask)
             else:
